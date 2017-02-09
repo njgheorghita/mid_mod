3fcc2194cc2febe7e45e48bc9640a94b4892b1d1
njgheorghita <njgheorghita@email.wm.edu>
2017-02-09 10:05:45 -0700

    Write search index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,5 @@
 class SearchController < ApplicationController
   def index
-    response = EventfulService.search_events_by_zip(params[:q])
-    events = JSON.parse(response.body)
-    @events_array = events["events"]["event"]
+    @response = EventfulService.search_events_by_zip(params[:q])
   end
 end
--- app/services/eventful_service.rb
+++ app/services/eventful_service.rb
@@ -1,5 +1,6 @@
 class EventfulService
   def self.search_events_by_zip(zip_code)
-    Faraday.get("http://api.eventful.com/json/events/search?app_key=9WC2RLPgZsMPmppm&location=#{zip_code}&date=March&sort_order=popularity&event_size=16")
+    response = Faraday.get("http://api.eventful.com/json/events/search?app_key=9WC2RLPgZsMPmppm&location=#{zip_code}&date=March&sort_order=popularity&event_size=16")
+    JSON.parse(response.body, symbolize_names: true)[:events][:event]
   end
 end
