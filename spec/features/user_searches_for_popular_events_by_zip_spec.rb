5c9d8ffc389feaad8b6abf3510a722bb5ba648ec
njgheorghita <njgheorghita@email.wm.edu>
2017-02-09 10:52:16 -0700

    Time to commit

--- spec/features/user_searches_for_popular_events_by_zip_spec.rb
+++ spec/features/user_searches_for_popular_events_by_zip_spec.rb
@@ -10,9 +10,10 @@ describe "As a User" do
       click_on "Find Events"
 
       expect(current_path).to eq("/search")
-      expect(page).to have_content("16 events for March") 
+      expect(page).to have_content("16 Events for March") 
       page.should have_selector('table tr', :count => 17)
       # For each event I can see the Event name, date & time, description (shortened to 140 characters), venue name (url to the venue page), venue address.
+      within first('.event') do 
     end
   end
-end
+end 
