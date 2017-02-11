73890dc408d8471294234ef4259bca08e8217ab9
njgheorghita <njgheorghita@email.wm.edu>
2017-02-11 10:28:58 -0700

    Add serialized Faraday request

--- app/services/eventful_service.rb
+++ app/services/eventful_service.rb
@@ -1,6 +1,15 @@
 class EventfulService
   def self.search_events_by_zip(zip_code)
-    response = Faraday.get("http://api.eventful.com/json/events/search?app_key=#{ENV["EVENTFUL_APP_KEY"]}&location=#{zip_code}&date=March&sort_order=popularity&page_size=16")
+    conn = Faraday.new("http://api.eventful.com/json/events/search")
+    
+    response = conn.get do |req| 
+      req.params['app_key'] = ENV["EVENTFUL_APP_KEY"]
+      req.params['location'] = zip_code
+      req.params['date'] = "March"
+      req.params['sort_order'] = "popularity"
+      req.params['page_size'] = 16
+    end
+
     JSON.parse(response.body, symbolize_names: true)[:events][:event]
   end
 end
